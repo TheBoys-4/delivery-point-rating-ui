7ce93656a4f96a683424ab723e863a4eb5fec7c7
CreatoR750 <nokia8951@mail.ru>
2023-05-23 23:23:01 +0300

    Adaptive

--- src/components/VortexBox/VortexBox.tsx
+++ src/components/VortexBox/VortexBox.tsx
@@ -1,16 +1,25 @@
 import React, { FC } from "react";
 import { Vortex } from "../../shared/assets";
-import './VortexBox.scss';
+import "./VortexBox.scss";
 
 export const VortexBox: FC = () => {
-    return (
-        <>
-            <div id="firstVortex">
-                <Vortex />
-            </div>
-            <div id="secondVortex">
-                <Vortex />
-            </div>
-        </>
-    )
-}
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "100%",
+        overflow: "hidden",
+      }}
+    >
+      <div id="firstVortex">
+        <Vortex />
+      </div>
+      <div id="secondVortex">
+        <Vortex />
+      </div>
+    </div>
+  );
+};
