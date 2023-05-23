15836816db1592b551d62e6925be7c0656d20098
guzenko_am <guzenko_am@nlmk.com>
2023-05-23 23:21:05 +0300

    adaptive

--- src/components/VortexBox/VortexBox.tsx
+++ src/components/VortexBox/VortexBox.tsx
@@ -4,13 +4,13 @@ import './VortexBox.scss';
 
 export const VortexBox: FC = () => {
     return (
-        <>
+        <div className="vortexBox">
             <div id="firstVortex">
                 <Vortex />
             </div>
             <div id="secondVortex">
                 <Vortex />
             </div>
-        </>
+        </div>
     )
 }
