f99dcdf61b65032445e17f261a94f5ee4a3722ba
guzenko_am <guzenko_am@nlmk.com>
2023-05-28 22:13:45 +0300

    admin

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,5 +1,5 @@
 import { Button, Upload, UploadProps } from "antd";
-import { Admin, Resource, ListGuesser, AppBar, Layout } from "react-admin"
+import {Admin, Resource, List, AppBar, Layout, ResourceProps, Datagrid, TextField, NumberField, Edit, SimpleForm, TextInput, NumberInput} from "react-admin"
 import { dataProvider } from "./helpers";
 import { AppBarProps } from "ra-ui-materialui/dist/cjs/layout/AppBar";
 import {DownloadOutlined, UploadOutlined } from "@ant-design/icons";
@@ -8,7 +8,7 @@ import {getXlsx} from "../../api/requests";
 
 const uploadProps: UploadProps = {
     name: 'file',
-    action: `http://localhost:4000/api/delivery-point-rating/data-service/import/xlsx`,
+    action: `${BASE_URL}import/xlsx`,
     onChange: (info) => {
         if (info.file.status !== 'uploading') {
             console.log(info.file, info.fileList);
@@ -26,6 +26,10 @@ const MyAppBar = (props: AppBarProps) => {
         getXlsx();
     }
 
+    const onGrafana = () => {
+        window.open('http://87.242.124.151:3001/', '_blank');
+    }
+
     return <AppBar {...props}>
         <Upload {...uploadProps}>
             <Button icon={<UploadOutlined />}>Import</Button>
@@ -33,7 +37,38 @@ const MyAppBar = (props: AppBarProps) => {
         <Button icon={<DownloadOutlined />} onClick={onDownload}>
             Download
         </Button>
+        <Button onClick={onGrafana}>
+            Grafana
+        </Button>
     </AppBar>
+}
+
+const MyList: ResourceProps['list'] = (props) => {
+    return (
+        <List {...props}>
+            <Datagrid rowClick="edit">
+                <TextField source="id" />
+                <NumberField source="dateTime" />
+                <NumberField source="score" />
+                <TextField source="messageSource" />
+                <TextField source="text" />
+            </Datagrid>
+        </List>
+    )
+}
+
+const MyEdit: ResourceProps['edit'] = (props) => {
+    return (
+        <Edit {...props}>
+            <SimpleForm>
+                <TextInput source="id" />
+                <NumberInput source="dateTime" />
+                <NumberInput source="score" />
+                <TextInput source="messageSource" />
+                <TextInput source="text" />
+            </SimpleForm>
+        </Edit>
+    )
 }
 
 
@@ -42,7 +77,6 @@ const MyLayout: typeof Layout = (props) => <Layout {...props} appBar={MyAppBar} 
 
 export const AdminPage = () => {
     return <Admin basename="/admin" layout={MyLayout} dataProvider={dataProvider}>
-        <Resource name="messages" list={ListGuesser} />
-        <Resource name="messageTypes" list={ListGuesser} />
+        <Resource name="messages" list={(props) => <MyList {...props} pagination={false} />} edit={MyEdit}  />
     </Admin>
 }
