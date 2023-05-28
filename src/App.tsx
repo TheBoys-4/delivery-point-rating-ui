a48b7be3d22f3262e708c19034175e4cbf0ea078
guzenko_am <guzenko_am@nlmk.com>
2023-05-28 15:35:43 +0300

    main"

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import { Rating } from "./pages/Rating";
 import { paths, roles } from "./shared/constants";
 import { AdminPage } from "./pages/AdminPage";
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter basename={paths.MAIN}>
         <Routes>
             {roles.user || roles.postomat ?  <Route path={paths.MAIN} element={<Rating />} /> : <></>}
             {roles.admin ? <Route  path={paths.ADMIN} element={<AdminPage />} /> : <></>}
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
