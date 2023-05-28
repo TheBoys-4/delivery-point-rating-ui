dbb276b2f611232dc39e6e8a5f24804c90b82209
guzenko_am <guzenko_am@nlmk.com>
2023-05-28 14:09:29 +0300

    admin

--- src/shared/constants/urls.ts
+++ src/shared/constants/urls.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
     "http://87.242.124.151:8080/api/delivery-point-rating/data-service/";
 
-export const DOMAIN = 'host.docker.internal:3000';
+export const DOMAIN = 'host.docker.internal:8080';
